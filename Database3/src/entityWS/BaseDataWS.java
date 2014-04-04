580bfc243d6b93925144a8d88b507a3d927ba200
rednanor <ronan.monahan@gmail.com>
2014-04-04 19:04:33 +0100

    Update BaseDataWS.java
    added Query

--- Database3/src/entityWS/BaseDataWS.java
+++ Database3/src/entityWS/BaseDataWS.java
@@ -12,7 +12,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-
+import javax.persistence.Query;
 import entities.BaseData;
 import entities.CellTable;
 import entities.Failure;
